586e128c33e9ee360c8458f7a7255f78c1dee819
Richard Rodger <richard@ricebridge.com>
2015-05-11 00:41:01 +0100

    country monitoring

--- salestax-monitor.js
+++ salestax-monitor.js
@@ -1,6 +1,7 @@
 
-var seneca = require('seneca')()
+var seneca = require('seneca')({timeout:555})
       .client({ pin:'role:salestax,cmd:calculate', port:51001 })
+      .client({ pin:'role:salestax,cmd:calculate,country:*', port:51002 })
 
 var influx = require('influx')({
   host:     'localhost',
@@ -10,19 +11,24 @@ var influx = require('influx')({
 })
 
 
-var expected = {
-  basic: 120
+function check(test) {
+  return function(err,out) {
+    influx.writePoint(
+      'salestax_calculate_'+test.name, 
+      { time:new Date(), value:(!err && test.val===out.total)?1:0 }, 
+      function(err){ if(err) console.log(err); })
+  }
 }
 
 setInterval(function(){
 
-  // basic
-  seneca.act('role:salestax,cmd:calculate,net:100',function(err,out){
-    influx.writePoint(
-      'salestax_calculate', 
-      { time:new Date(), value:(!err && expected.basic===out.total)?1:0 }, 
-      function(err){ if(err) console.log(err); })
-  })
+  seneca
+
+    .act('role:salestax,cmd:calculate,net:100',
+         check({name:'basic',val:120}))
+
+    .act('role:salestax,cmd:calculate,country:ie,net:100',
+         check({name:'ie',val:121}))
 
 },555)
 
