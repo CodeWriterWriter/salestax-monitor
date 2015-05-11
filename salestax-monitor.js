db01d9fd4b959d2e4959ae011109555209a33303
Richard Rodger <richard@ricebridge.com>
2015-05-11 02:42:45 +0100

    basic and country

--- salestax-monitor.js
+++ salestax-monitor.js
@@ -16,7 +16,7 @@ var influx = require('influx')({
 var HOST = process.env.HOST || 'localhost' 
 var PORT = process.env.PORT || 51000 
 
-function check(test) {
+function check_service(test) {
   return function(err,out) {
     influx.writePoint(
       'salestax_calculate_'+test.name, 
@@ -25,25 +25,34 @@ function check(test) {
   }
 }
 
+function check_api(test) {
+  return function(err,res){
+    var out = res && res.body ? res.body : {}
+    influx.writePoint(
+      'salestax_api_'+test.name, 
+      { time:new Date(), value:(!err && test.val===out.total)?1:0 }, 
+      function(err){ if(err) console.log(err); })
+  }
+}
+
+
 setInterval(function(){
 
   seneca
 
     .act('role:salestax,cmd:calculate,net:100',
-         check({name:'basic',val:120}))
+         check_service({name:'basic',val:120}))
 
     .act('role:salestax,cmd:calculate,country:ie,net:100',
-         check({name:'ie',val:121}))
+         check_service({name:'ie',val:121}))
 
   needle.get(
     'http://'+HOST+':'+PORT+'/api/salestax?net=100',
-    function(err,res){
-      var out = res && res.body ? res.body : {}
-      influx.writePoint(
-        'salestax_api_ok', 
-        { time:new Date(), value:(!err && 120===out.total)?1:0 }, 
-        function(err){ if(err) console.log(err); })
-    })
+    check_api({name:'basic',val:120}))
+
+  needle.get(
+    'http://'+HOST+':'+PORT+'/api/salestax?net=100&country=de',
+    check_api({name:'uk',val:119}))
 
 },555)
 
