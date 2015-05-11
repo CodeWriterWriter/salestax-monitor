b1a2cc0cb670172f7e0c0f5e6e39f274d1185e66
Richard Rodger <richard@ricebridge.com>
2015-05-11 14:57:32 +0100

    beanstalk

--- salestax-monitor.js
+++ salestax-monitor.js
@@ -1,6 +1,14 @@
+var BS_PORT  = process.env.BS_PORT  || 11300
+var BS_HOST  = process.env.BS_HOST  || 'localhost'
 
 var seneca = require('seneca')()
-      .client({ pin:'role:salestax,cmd:calculate', port:51001 })
+      .use('beanstalk-transport')
+      .client({
+        type: 'beanstalk',
+        pin:  'role:salestax,cmd:calculate',
+        port:  BS_PORT,
+        host:  BS_HOST
+      })
 
 var influx = require('influx')({
   host:     'localhost',
@@ -19,7 +27,7 @@ setInterval(function(){
   // basic
   seneca.act('role:salestax,cmd:calculate,net:100',function(err,out){
     influx.writePoint(
-      'salestax_calculate', 
+      'salestax_calculate_basic', 
       { time:new Date(), value:(!err && expected.basic===out.total)?1:0 }, 
       function(err){ if(err) console.log(err); })
   })
